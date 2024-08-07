970b4b085c1284632abe18e95a3d221e63d26165
Lucas Leitão <102380022+LucasLeitao-MSFT@users.noreply.github.com>
2024-08-07 11:31:18 +0100

    Add files via upload

---
+++ GetGroupsWhoHaveGroupsAsMembers.ps1
@@ -0,0 +1,30 @@
+﻿#Insta MG Graph Module
+Install-Module Microsoft.Graph -Scope CurrentUser
+
+#Connect with the correct scope
+Connect-MgGraph -Scopes "Group.Read.All"
+
+#Initializing variables
+$groupsWithNestedGroups = @()
+$groups = Get-MgGroup -all
+
+
+#Going through all groups and getting the respective group members#If one of group's members is another group, group object is added to the $groupsWithNestedGroups variable. 
+foreach ($group in $groups) {
+    $members = Get-MgGroupMember -GroupId $group.Id -All
+    foreach ($member in $members) {
+        if ($member.additionalproperties.'@odata.type' -eq "#microsoft.graph.group") {
+            $groupsWithNestedGroups += $group
+            break
+        Write-Output $member.Id
+        }
+
+    }
+}
+
+$groupsWithNestedGroups | Select-Object DisplayName, Id
+
+
+
+
+
