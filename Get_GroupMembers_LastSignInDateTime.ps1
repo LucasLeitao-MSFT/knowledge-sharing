dc319fd6c2ee0c1b2e178ff72446a52bd54db4bc
Lucas Leitão <102380022+LucasLeitao-MSFT@users.noreply.github.com>
2024-11-25 13:57:31 +0000

    Add files via upload

---
+++ Get_GroupMembers_LastSignInDateTime.ps1
@@ -0,0 +1,19 @@
+﻿#Conenct to MgGraph with the correct scopes
+Connect-MgGraph -Scopes "User.Read.All","Group.Read.All","AuditLog.Read.All"
+
+
+# Retrieve all members of the group
+$groupId = '<Group_ObjectID>'
+$groupMembers = Get-MgGroupMember -GroupId $groupId
+
+# Loop through each member and retrieve additional properties
+foreach ($member in $groupMembers) {
+    $user = Get-MgUser -UserId $member.Id -Select id, displayName, userPrincipalName,SigninActivity
+    # Output or process the user information as needed
+    Write-Output "User ID: $($user.Id)"
+    Write-Output "Display Name: $($user.DisplayName)"
+    Write-Output "User Principal Name: $($user.UserPrincipalName)"
+    Write-Output "Last SignInDateTime: $($user.signinactivity.lastSignInDateTime)"
+}
+
+
